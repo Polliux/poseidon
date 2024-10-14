48de9ed57bff87df9a107f190af51762d83a3c29
USER <jacko923752@gmail.com>
2024-10-14 18:39:36 +0800

    camera

---
+++ src/scripts/camera_2d.gd
@@ -0,0 +1,52 @@
+extends Camera2D
+
+const FRAMERATE_CONST = 60
+const ZOOM_MAX: Vector2 = Vector2(1.4,1.4)
+const ZOOM_MIN: Vector2 = Vector2(0.6,0.6)
+const ZOOM_INCREMENT: float = 0.2
+const ZOOM_SPEED: float = 0.3 * FRAMERATE_CONST
+
+const CAMERA_LERP_SPEED: float = 0.05 * FRAMERATE_CONST
+
+var zoom_enabled = true
+var zoom_cd: int = 10
+var target_zoom: Vector2 = Vector2(1,1)
+
+var target_position: Vector2
+
+
+func _ready():
+	pass 
+
+func _process(delta):
+	zoom_cd = clampi(zoom_cd-1,0,10)
+	
+	# Target Position
+	if not target_position.is_equal_approx(self.position):
+		self.position = lerp(self.position,target_position,CAMERA_LERP_SPEED*delta)
+			
+	# Target Zoom
+	if not target_zoom.is_equal_approx(self.zoom):
+		if self.zoom < target_zoom:
+			self.zoom = lerp(self.zoom,target_zoom,ZOOM_SPEED*delta)
+			#print(self.zoom)
+
+		elif self.zoom > target_zoom:
+			self.zoom = lerp(self.zoom,target_zoom,ZOOM_SPEED*delta)
+			#print(self.zoom)
+
+
+func on_mousewheel(mousewheel_direction: int) -> void:
+	# 1 -> UP, -1 -> DOWN
+		
+	if zoom_enabled and zoom_cd <= 0:
+		if mousewheel_direction > 0 and target_zoom < ZOOM_MAX:
+				target_zoom += Vector2(ZOOM_INCREMENT,ZOOM_INCREMENT)
+
+		elif mousewheel_direction < 0 and target_zoom > ZOOM_MIN:
+				target_zoom += Vector2(-ZOOM_INCREMENT,-ZOOM_INCREMENT)
+
+		zoom_cd = 3
+
+func mouse_drag_camera(vector:Vector2) -> void:
+	target_position -= (2*vector)
--- src/scripts/map.gd
+++ src/scripts/map.gd
@@ -4,8 +4,13 @@ extends Node2D
 @onready var stilemap = $SelectionTML
 @onready var camera = $Camera2D
 
-var zoom_level: int = 1
-var zoom_cd: int = 30
+const CONSIDER_AS_DRAG_TRESHOLD: float = 3
+var mouse_click_pos: Vector2 = Vector2(0,0)
+var mouse_release_pos: Vector2 = Vector2(0,0)
+var leftclick_release_cd: int = 30
+
+var mouse_hold: bool = false
+var camera_dragging: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,36 +18,57 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	
+	leftclick_release_cd = clampi(leftclick_release_cd-1,0,10)
+	
+	# Cursor Selection
 	stilemap.clear()
 	var cursorpos: Vector2 = get_global_mouse_position()
-	stilemap.set_cell(stilemap.local_to_map(cursorpos),0,Vector2i(1,0))
+	stilemap.set_cell(stilemap.local_to_map(cursorpos),1,Vector2i(1,0))
 	
-	zoom_cd = clampi(zoom_cd-1,0,100)
-	pass
+	if mouse_hold:
+		#print(leftclick_hold_time)
+		camera_dragging = true
+	else:
+		camera_dragging = false
+
 
 func _input(event):
-	if Input.is_action_just_pressed("Left Click"):
-		print("map coords: ")
-		print(tilemap.local_to_map(get_global_mouse_position()))
-		
-		## test building add
-		var wm = preload("res://src/scenes/wm_anim.tscn")
-		var new_wm = wm.instantiate()
-		new_wm.position = tilemap.map_to_local(tilemap.local_to_map(get_global_mouse_position()))
-		add_child(new_wm)
-		
-	if zoom_cd <= 0:
-		if Input.is_action_just_pressed("Mousewheel - Up"):
-			zoom_level = clampi(zoom_level+1,1,3)
-			change_zoom()
-			zoom_cd = 10
-		elif Input.is_action_just_pressed("Mousewheel - Down"):
-			zoom_level = clampi(zoom_level-1,1,3)
-			change_zoom()
-			zoom_cd = 10
 	
-func change_zoom() -> void:
-	print(zoom_level)
-	var zoom_value: float = 0.5 + (zoom_level * 0.5)
-	camera.set_zoom(Vector2(zoom_value,zoom_value))
+	# MOUSECLICK
+	if Input.is_action_just_released("Left Click") and (leftclick_release_cd <= 0):
+		mouse_release_pos = get_global_mouse_position()
+		leftclick_release_cd = 10
+		mouse_hold = false
+		if (mouse_click_pos.distance_to(mouse_release_pos) < CONSIDER_AS_DRAG_TRESHOLD ):
+			# INTEPRET AS CLICK
+			add_test_building(get_global_mouse_position())
+
+		camera_dragging = false
+		
+	elif Input.is_action_just_pressed("Left Click"):
+		mouse_click_pos = get_global_mouse_position()
+		mouse_hold = true
+	
+	# MOUSE MOTION
+	if camera_dragging:
+		if event is InputEventMouseMotion:
+			print(event.relative)
+			camera.mouse_drag_camera(event.relative)
+
+	# MOUSEWHEEL
+	if Input.is_action_just_pressed("Mousewheel - Up"):
+		camera.on_mousewheel(1)
+	elif Input.is_action_just_pressed("Mousewheel - Down"):
+		camera.on_mousewheel(-1)
+	
+func add_test_building(mouse_position:Vector2):
+	print("map coords: ")
+	print(tilemap.local_to_map(mouse_position))
+	
+	## test building add
+	var wm = preload("res://src/scenes/wm_anim.tscn")
+	var new_wm = wm.instantiate()
+	new_wm.position = tilemap.map_to_local(tilemap.local_to_map(mouse_position))
+	add_child(new_wm)
